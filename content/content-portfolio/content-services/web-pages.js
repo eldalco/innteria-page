c1c85d41d12c65bb1dba7522d0424332fc7a88d5
eldalco <elkincortes03@gmail.com>
2022-07-28 17:42:19 -0500

    Content files for Each section

---
+++ content/content-portfolio/content-services/web-pages.js
@@ -0,0 +1,132 @@
+const data = [
+    {
+        id: "0",
+        name: "mbs",
+        logo: "/img/portfolio/logo-mbs.png",
+        img: "/img/portfolio/mbs.webp",
+        location: "Miami. United States",
+        urlSite: "https://mbsrepair.com/"
+    },
+    {
+        id: "1",
+        name: "casa-concreto",
+        logo: "/img/portfolio/logo-macondo.png",
+        img: "/img/portfolio/macondo.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://vivemacondo.com/"
+    },
+    {
+        id: "2",
+        name: "ludica",
+        logo: "/img/portfolio/logo-ludica.png",
+        img: "/img/portfolio/ludica.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://www.ludicatecnolyte.com/"
+    },
+    {
+        id: "3",
+        name: "melomano",
+        logo: "/img/portfolio/logo-melomano.png",
+        img: "/img/portfolio/melomanorecords.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://melomanorecords.com/"
+    },
+    {
+        id: "4",
+        name: "omp",
+        logo: "/img/portfolio/logo-omp.png",
+        img: "/img/portfolio/omp.webp",
+        location: "Querétaro, México",
+        urlSite: "https://omp.mx"
+    },
+    {
+        id: "5",
+        name: "casa-concreto",
+        logo: "/img/portfolio/logo-casa-concreto.png",
+        img: "/img/portfolio/casa-concreto.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://www.casaconcreto.com.co/"
+    },
+    {
+        id: "6",
+        name: "synchronicity",
+        logo: "/img/portfolio/logo-synchronicity.png",
+        img: "/img/portfolio/synchronicity.webp",
+        location: "México",
+        urlSite: "https://synchronicity.com.mx"
+    },
+    {
+        id: "7",
+        name: "aurapuncture",
+        logo: "/img/portfolio/logo-aurapuncture.png",
+        img: "/img/portfolio/aurapuncture.webp",
+        location: "Miami. United States",
+        urlSite: "https://acupunctureofmiami.com"
+    },
+    {
+        id: "8",
+        name: "elegant-yachting",
+        logo: "/img/portfolio/logo-elegant-yachting.png",
+        img: "/img/portfolio/elegant-yachting.webp",
+        location: "Miami. United States",
+        urlSite: "https://elegantyachtingmiami.com"
+    },
+    {
+        id: "9",
+        name: "nativus-coffe",
+        logo: "/img/portfolio/logo-nativus.png",
+        img: "/img/portfolio/nativus.webp",
+        location: "Colombia",
+        urlSite: "https://nativuscoffee.com"
+    },
+    {
+        id: "10",
+        name: "gmi",
+        logo: "/img/portfolio/logo-gmi.png",
+        img: "/img/portfolio/gmi.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://globalmasterinvestment.com"
+    },
+    {
+        id: "11",
+        name: "arqinnovation",
+        logo: "/img/portfolio/logo-arqinnovation.png",
+        img: "/img/portfolio/arqinnovation.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://arqinnovation.com"
+    },
+    {
+        id: "12",
+        name: "aureomarkets",
+        logo: "/img/portfolio/logo-aureomarkets.png",
+        img: "/img/portfolio/aureomarkets.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://www.aureomarkets.com"
+    },
+    {
+        id: "13",
+        name: "walter",
+        logo: "/img/portfolio/logo-walter.png",
+        img: "/img/portfolio/walter.webp",
+        location: "Argentina",
+        urlSite: "https://walterjoaquinhurtado.com"
+    },
+    {
+        id: "14",
+        name: "secsas",
+        logo: "/img/portfolio/logo-secsas.png",
+        img: "/img/portfolio/secsas.webp",
+        location: "Bogotá. Colombia",
+        urlSite: "https://secsas.co"
+    },
+    {
+        id: "15",
+        name: "taylor",
+        logo: "/img/portfolio/logo-taylor.png",
+        img: "/img/portfolio/taylor.webp",
+        location: "Florida. United States",
+        urlSite: "https://www.taylordyachtcharters.com"
+    },
+    
+]
+export default data;
---
+++ content/slider-ser-content.js
@@ -0,0 +1,31 @@
+const dataSlider = [
+    {
+        id: "0",
+        title: "Servicio de Plataformas web.",
+        slogan: "coding a better future",
+        content: "Servicio ideal si necesitas sistematizar, automatizar o recolectar la información de los procesos de tu empresa y acceder a ellos en cualquier lugar que tengas conexión a internet.",
+        imgUrl: "/img/Innteria_PlataformasWeb.webp"
+    },
+    {
+        id: "1",
+        title: "Servicio de Páginas web.",
+        slogan: "coding a better future",
+        content: "Servicio ideal si tu empresa necesita tener posicionamiento y visibilidad en el mundo digital, adicionalmente es un canal de recolección de potenciales clientes.",
+        imgUrl: "/img/Innteria_web-page.webp"
+    },
+    {
+        id: "2",
+        title: "Analítica de datos.",
+        slogan: "coding a better future",
+        content: "Es perfecto si necesitas estructurar los datos recolectados en tu empresa, para así tomar decisiones más acertadas.",
+        imgUrl: "/img/Innteria_Data.webp"
+    },
+    {
+        id: "3",
+        title: "Inteligencia Artificial.",
+        slogan: "coding a better future",
+        content: "Es el servicio que necesitas si tu empresa quiere estar a la vanguardia usando modelos predictivos y aprendizaje automático.",
+        imgUrl: "/img/Innteria_IA.webp"
+    }
+];
+export default dataSlider;
